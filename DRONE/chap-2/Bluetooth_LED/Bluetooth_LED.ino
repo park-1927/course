c06cbc6f19140dc11a0d8c2ae6f1f6190f4be2a5
park-1927 <signlp12@gmail.com>
2025-03-05 13:49:38 +0900

    Add files via upload

---
+++ DRONE/chap-2/Bluetooth_LED/Bluetooth_LED.ino
@@ -0,0 +1,28 @@
+#Bluetooth_LED
+#include <SoftwareSerial.h>
+SoftwareSerial bluetooth(8, 9); // 8번 TX, 9번 RX
+void setup()
+{
+  Serial.begin(9600);
+  bluetooth.begin(9600);
+  pinMode(3, OUTPUT); // LED OUTPUT
+  pinMode(5, OUTPUT); // LED OUTPUT
+}
+
+void loop()
+{
+  char val = bluetooth.read();
+  if (bluetooth.available()){
+    Serial.write(bluetooth.read());
+  }
+  if(val == 'a'){
+    digitalWrite(3,HIGH);
+  }
+  if(val == 'b'){
+    digitalWrite(5, HIGH);
+  }
+  else if(val == 'c'){
+    digitalWrite(3, LOW);
+    digitalWrite(5, LOW);
+  }
+}
